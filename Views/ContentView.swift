0a3b6b003771432721d8f87ddd2c6b1941f118f0
James Baker <jbaker00@gmail.com>
2022-02-21 10:22:36 -0800

    Added code for the initial screens

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -9,14 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("My tasks")
-                .font(.title3).bold()
-                .padding()
-                .frame(maxWidth: .infinity, alignment: .leading)
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color(hue: 0.086, saturation: 0.141, brightness: 0.972))
+        Text("Hello world!")
+            .padding()
     }
 }
 
--- Views/TaskView.swift
+++ Views/TaskView.swift
@@ -5,4 +5,23 @@
 //  Created by James Baker on 2/20/22.
 //
 
-import Foundation
+import SwiftUI
+
+struct TaskView: View {
+    var body: some View {
+        VStack {
+            Text("My tasks")
+                .font(.title3).bold()
+                .padding()
+                .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(hue: 0.086, saturation: 0.141, brightness: 0.972))
+    }
+}
+
+struct TaskView_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskView()
+    }
+}
