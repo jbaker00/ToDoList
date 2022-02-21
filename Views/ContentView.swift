8ad36cb5ac1d4eb769a2cd7971ee73e39fdac986
James Baker <jbaker00@gmail.com>
2022-02-21 10:40:36 -0800

    Adding Realm

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showAddTaskView = false
     var body: some View {
-        Text("Hello world!")
-            .padding()
+        ZStack(alignment: .bottomTrailing){
+            TaskView()
+            
+            SmallAddButton()
+                .padding()
+                .onTapGesture {
+                    showAddTaskView.toggle()
+                }
+        }
+        .sheet(isPresented: $showAddTaskView){
+                AddTaskView()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(hue: 0.086, saturation: 0.141, brightness: 0.972))
     }
 }
 
